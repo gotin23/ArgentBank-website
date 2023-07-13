2ff1b7ffb50f3ed9e6ff3f92c545d7802c85a9fe
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-07-13 20:53:49 +0200

    reorganisation du projet et initialisation du front avec cra

---
+++ p11/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./layouts/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hey</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
